6a767fa763bf44398e86a9b613c3d32d9954b843
Matt Glaman <nmd.matt@gmail.com>
2016-06-20 16:14:17 -0500

    Fix the runner scripts

--- scripts/migrate-ck2.sh
+++ scripts/migrate-ck2.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-
-drush migrate-upgrade --legacy-db-url=mysql://root:root@mysql.dev:32783/ck2_migrate --legacy-root=http://ck2migrate.com --root=$(pwd)/web
+PORT=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "3306/tcp") 0).HostPort}}' commercemigrateenvironment_mariadb_1)
+drush migrate-upgrade --legacy-db-url=mysql://root:root@mysql.dev:${PORT}/ck2_migrate --legacy-root=http://ck2migrate.com --root=$(pwd)/web

26d36ce98f81b6f16163de8d1c873365ee4fe496
Matt Glaman <nmd.matt@gmail.com>
2016-06-20 16:46:03 -0500

    UC6 script pointing to wrong directory

--- scripts/migrate-uc6.sh
+++ scripts/migrate-uc6.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 PORT=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "3306/tcp") 0).HostPort}}' commercemigrateenvironment_mariadb_1)
-drush migrate-upgrade --legacy-db-url=mysql://root:root@mysql.dev:${PORT}/ck2_migrate --legacy-root=http://ck2migrate.com --root=$(pwd)/web
+drush migrate-upgrade --legacy-db-url=mysql://root:root@mysql.dev:${PORT}/uc6_migrate --legacy-root=http://ck2migrate.com --root=$(pwd)/web
